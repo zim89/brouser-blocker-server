7a2d3af26cd47fbe447fffb5ab4b903ba2874159
zim89 <121241841+zim89@users.noreply.github.com>
2024-03-29 20:21:57 +0200

    add prisma, docker, dbModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import {ApiOkResponse, ApiProperty} from "@nestjs/swagger";
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger'
+import { DbService } from './db/db.service'
 
 class HelloWorldDto {
   @ApiProperty()
-  message: string;
+  message: string
 }
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
   @ApiOkResponse({
     type: HelloWorldDto,
   })
-  getHello(): HelloWorldDto {
-    return {message: this.appService.getHello()};
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await this.dbService.user.findMany()
+    console.log(users)
+    return { message: this.appService.getHello() }
   }
 }
